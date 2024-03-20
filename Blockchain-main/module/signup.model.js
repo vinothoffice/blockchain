2950d685889f215bc4af3cdc800152b75dd39319
vinothoffice <vinoth@medpick.in>
2024-03-20 12:12:28 +0530

    updated

--- Blockchain-main/module/signup.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-    email:{type:String,require:true},
-    password:{type:String,require:true},
-    role:{type:String,require:false}
-})
-
-const userModel = mongoose.model("z", userSchema)
-module.exports = {userModel}
